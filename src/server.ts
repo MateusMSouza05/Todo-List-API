9e510705c79af35feb4e86e8a6f95130eb1d0328
Mateus Macedo <mateusmacedobatista05@gmail.com>
2026-02-18 20:09:34 -0300

    fix: update import path for todoRoutes in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import 'dotenv/config';
-import routes from './routes';
+import routes from './routes/todoRoutes';
 
 const app = express();
 const port = process.env.PORT || 3000;
