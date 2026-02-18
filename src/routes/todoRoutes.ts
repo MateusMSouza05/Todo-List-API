5b4b68795665bb797c9927b956f41b986940f241
Mateus Macedo <mateusmacedobatista05@gmail.com>
2026-02-18 20:16:10 -0300

    feat: add getTodos method to retrieve all todos

--- src/routes/todoRoutes.ts
+++ src/routes/todoRoutes.ts
@@ -6,5 +6,6 @@ const todoController = new TodoController();
 
 router.post('/todos', todoController.createTodo);
 router.put('/todos/:id', todoController.updateTodo);
+router.get('/todos', todoController.getTodos);
 
 export default router;
