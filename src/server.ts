9395eb3c2a35de0c18e5c7cb6475bd897035d048
Mateus Macedo <mateusmacedobatista05@gmail.com>
2026-02-18 19:08:23 -0300

    chore: initate repo

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import 'dotenv/config';
+import routes from './routes';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
